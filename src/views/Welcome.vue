4d87f6fccc6fc5272cff4ad54fca2f3f1046a582
Massimiliano <massimiliano.porzio@gmail.com>
2021-10-09 16:54:20 +0200

    Firdst commit project setup

---
+++ src/views/Welcome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="welcome container">
+   <p>Welcome</p>
+  </div>
+</template>
+
+<script setup>
+const name = 'Home'
+
+
+</script>
+
+<style>
+.welcome {
+  text-align: center;
+  padding: 20px 0;
+}
+</style>
