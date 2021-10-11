e748f0cd6319ddd37cf8c5683e3a7b6556ae3a84
Massimiliano <massimiliano.porzio@gmail.com>
2021-10-11 20:28:06 +0200

    navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav>
+    <div>
+      <p>Hey there... display name here</p>
+      <p class="email">Currenty logged in as ... email</p>
+    </div>
+    <button>Logout</button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style>
+nav {
+  padding: 20px;
+  border-bottom: 1px solid #eee;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+nav p {
+  margin: 2px auto;
+  font-size: 16px;
+  color: #444;
+}
+nav p.email{
+  color: #999;
+  font-size: 14px;
+}
+</style>
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,10 +1,15 @@
 <template>
-<h1>Chat</h1>
+  <div class="container">
+    <Navbar/>
+  </div>
+
 </template>
 
 <script>
+import Navbar from "@/components/Navbar";
 export default {
-  name: "Chatroom"
+  name: "Chatroom",
+  components: {Navbar}
 }
 </script>
 
